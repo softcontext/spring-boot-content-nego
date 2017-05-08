7a89afd537d10ab09e85bc7eee1a48123d477bf4
Seokwon SONG <softcontext@gmail.com>
2017-05-08 11:02:19 +0900

    just for fun

--- src/main/java/com/example/StartApplication.java
+++ src/main/java/com/example/StartApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartApplication {
+public class StartApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 }
